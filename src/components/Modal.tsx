ae3f65516ed9399341665646f3a8936a37318c67
Malber Josue Batista Cuello <mbatista@GDWN.local>
2024-01-22 17:35:38 -0400

    Cambios

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,17 +1,18 @@
 import React from "react";
+import { type User } from "../types.d";
 
-export default function Modal() {
+interface UserCardProps {
+    user: User;
+    isOpen: boolean;
+    onRequestClose: (event: Event) => void;
+}
+
+export default function Modal({ user, isOpen }: UserCardProps)  {
     const [showModal, setShowModal] = React.useState(false);
     return (
         <>
-            <button
-                className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={() => setShowModal(true)}
-            >
-                Open regular modal
-            </button>
-            {showModal ? (
+    
+            {isOpen ? (
                 <>
                     <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
                         <div className="relative w-auto my-6 mx-auto max-w-3xl">
@@ -19,7 +20,7 @@ export default function Modal() {
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                 {/*header*/}
                                 <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                                    <h3 className="text-3xl font-semibold">Modal Title</h3>
+                                    <h3 className="text-3xl font-semibold">Datos de usuario</h3>
                                     <button
                                         className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                                         onClick={() => setShowModal(false)}
@@ -32,11 +33,7 @@ export default function Modal() {
                                 {/*body*/}
                                 <div className="relative p-6 flex-auto">
                                     <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
-                                        I always felt like I could do anything. That’s the main
-                                        thing people are controlled by! Thoughts- their perception
-                                        of themselves! They're slowed down by their perception of
-                                        themselves. If you're taught you can’t do anything, you
-                                        won’t do anything. I was taught I could do everything.
+                                    {user.company.department}
                                     </p>
                                 </div>
                                 {/*footer*/}
